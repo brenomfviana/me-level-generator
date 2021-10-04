788d4f29c9152ce053fa5fc8ef7eb363b474d31f
brenov <bmfviana@gmail.com>
2021-10-04 15:11:50 -0300

    updates the mutation operator to transfer enemies between rooms

--- Mutation.cs
+++ Mutation.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace LevelGenerator
 {
@@ -14,6 +17,7 @@ namespace LevelGenerator
             ref Random _rand
         ) {
             Individual individual = _parent.Clone();
+            // Remove or add a lock and a key
             if (MUTATION_TYPE_RATE > Common.RandomPercent(ref _rand))
             {
                 individual.dungeon.AddLockAndKey(ref _rand);
@@ -21,6 +25,24 @@ namespace LevelGenerator
             else
             {
                 individual.dungeon.RemoveLockAndKey(ref _rand);
+            }
+            // Transfer enemies between rooms
+            int size = individual.dungeon.rooms.Count;
+            List<int> options = Enumerable.Range(1, size - 1).ToList<int>();
+            while (options.Count / 2 > 1)
+            {
+                // Select random rooms
+                int idx_f = Common.RandomElementFromList(options, ref _rand);
+                options.Remove(idx_f);
+                int idx_t = Common.RandomElementFromList(options, ref _rand);
+                options.Remove(idx_t);
+                // Transfer enemies
+                Room from = individual.dungeon.rooms[idx_f];
+                Room to = individual.dungeon.rooms[idx_t];
+                int transfer = Common.RandomInt((0, from.enemies), ref _rand);
+                from.enemies -= transfer;
+                to.enemies += transfer;
+                Debug.Assert(from.enemies >= 0 && to.enemies >= 0);
             }
             return individual;
         }
