c9e82c2591fee293113df109e66b216b5029aae3
brenov <bmfviana@gmail.com>
2021-10-05 14:22:13 -0300

    applies code refactoring in Fitness.cs

--- Fitness.cs
+++ Fitness.cs
@@ -6,56 +6,61 @@ namespace LevelGenerator
     /// This class holds all the fitness-related functions.
     public class Fitness
     {
-        /// Fitness is based in the number of rooms, number of keys and locks, the linear coefficient and the number of locks used by the A*
+        /// Fitness is based in the number of rooms, number of keys and locks, the linear coefficient and the number of locks used by the A*.
         public static void Calculate(
             Parameters _prs,
             ref Individual _individual,
             ref Random _rand
         ) {
+            // Create aliases for the individual's attributes
             Dungeon dungeon = _individual.dungeon;
-            int nV = _prs.rooms;
-            int nK = _prs.keys;
-            int nL = _prs.locks;
-            float lCoef = _prs.linearCoefficient;
-            float fit;
-            if (dungeon.lockIds.Count > 0 && dungeon.keyIds.Count == 0)
+            int rooms = dungeon.rooms.Count;
+            int keys = dungeon.keyIds.Count;
+            int locks = dungeon.lockIds.Count;
+            float linearCoefficient = _individual.linearCoefficient;
+            // Calculate the distance between the attributes of the generated
+            // dungeon to the entered parameters
+            float distance = Math.Abs(_prs.rooms - rooms) +
+                Math.Abs(_prs.keys - keys) +
+                Math.Abs(_prs.locks - locks) +
+                Math.Abs(_prs.linearCoefficient - linearCoefficient);
+            float fit = 2 * distance;
+            // If the level has locked doors
+            if (dungeon.lockIds.Count > 0)
             {
-                _individual.fitness = -1f;
-                return;
+                // Calculate the number of locks needed to finish the level
+                _individual.neededLocks = AStar.FindRoute(dungeon);
+                // Validate the calculated number of needed locks
+                if (_individual.neededLocks > dungeon.lockIds.Count)
+                {
+                    throw new Exception("Inconsistency! The number of " +
+                        "needed locks is higher than the number of total " +
+                        "locks of the level." +
+                        "\n  Total locks=" + dungeon.lockIds.Count +
+                        "\n  Needed locks=" + _individual.neededLocks);
+                }
+                // Calculate the number of rooms needed to finish the level
+                float neededRooms = 0f;
+                for (int i = 0; i < 3; i++)
+                {
+                    DFS dfs = new DFS(dungeon);
+                    dfs.FindRoute(dungeon, ref _rand);
+                    neededRooms += dfs.NVisitedRooms;
+                }
+                _individual.neededRooms = neededRooms / 3.0f;
+                // Validate the calculated number of needed rooms
+                if (_individual.neededRooms > dungeon.Rooms.Count)
+                {
+                    throw new Exception("Inconsistency! The number of " +
+                        "needed rooms is higher than the number of total " +
+                        "rooms of the level." +
+                        "\n  Total rooms=" + dungeon.Rooms.Count +
+                        "\n  Needed rooms=" + _individual.neededRooms);
+                }
+                // Update the fitness by summing the needed rooms and locks
+                fit += dungeon.lockIds.Count * 0.8f - _individual.neededLocks +
+                    dungeon.Rooms.Count - _individual.neededRooms;
             }
-            // If the level has no locked door nor key
-            if (dungeon.lockIds.Count == 0)
-            {
-                fit = 2*(System.Math.Abs(nV - dungeon.Rooms.Count) + System.Math.Abs(nK - dungeon.keyIds.Count) + System.Math.Abs(nL - dungeon.lockIds.Count) + System.Math.Abs(lCoef - _individual.linearCoefficient));
-                _individual.fitness = fit;
-                return;
-            }
-            float avgUsedRoom = 0.0f;
-            // Only use the A* if there is a lock in the dungeon
-            // System.Console.WriteLine("Begin A*");
-            _individual.neededLocks = AStar.FindRoute(dungeon);
-            for (int i = 0; i < 3; ++i)
-            {
-                DFS dfs = new DFS(dungeon);
-                dfs.FindRoute(dungeon, ref _rand);
-                avgUsedRoom += dfs.NVisitedRooms;
-            }
-            _individual.neededRooms = avgUsedRoom / 3.0f;
-            // System.Console.WriteLine("Needed Rooms: " + _individual.neededRooms);
-            if (_individual.neededRooms > dungeon.Rooms.Count)
-            {
-                Console.WriteLine("SOMETHING IS REALLY WRONG! Nrooms: " + dungeon.Rooms.Count + "  Used: " + _individual.neededRooms);
-                Console.ReadKey();
-            }
-            if (_individual.neededLocks > dungeon.lockIds.Count)
-            {
-                Console.WriteLine("SOMETHING IS REALLY WRONG!");
-                Console.WriteLine(_individual.neededLocks);
-                Console.WriteLine(dungeon.lockIds.Count);
-            }
-
-            fit = (2*(System.Math.Abs(nV - dungeon.Rooms.Count) + System.Math.Abs(nK - dungeon.keyIds.Count) + System.Math.Abs(nL - dungeon.lockIds.Count) + System.Math.Abs(lCoef - _individual.linearCoefficient)) + (dungeon.lockIds.Count * 0.8f - _individual.neededLocks) + (dungeon.Rooms.Count - _individual.neededRooms));
-
             _individual.fitness = fit;
         }
 
